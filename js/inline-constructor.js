30024475f6f61a54ef4f8d25f2c1975ac23cf259
andreytukubayev-cmd <andreytukubayev@gmail.com>
2026-06-22 21:53:44 +0500

    Update inline-constructor.js 2153

--- js/inline-constructor.js
+++ js/inline-constructor.js
@@ -132,10 +132,10 @@ function switchToSector(sectorIndex) {
         // Пересоздаем инпуты ТОЛЬКО если их физически нет в текущем box
         if (!inputKk || !inputRu) {
             box.innerHTML = `
-                <div style="display: flex; flex-direction: column; gap: 4px; width: 100%;">
-                    <input type="text" class="constructor-input-kk" style="width: 100%; background: #0f172a; color: ${layer.colorKk}; border: 1px solid #334155; padding: 4px 8px; font-size: 13px; font-weight: bold; border-radius: 4px; box-sizing: border-box;">
-                    <input type="text" class="constructor-input-ru" style="width: 100%; background: #0f172a; color: #cbd5e1; border: 1px solid #334155; padding: 4px 8px; font-size: 11px; border-radius: 4px; box-sizing: border-box;">
-                </div>
+			<div style="display: flex; flex-direction: column; gap: 4px; width: 100%;">
+            <input type="text" class="constructor-input-kk" autocomplete="off" style="width: 100%; background: #0f172a; color: ${layer.colorKk}; border: 1px solid #334155; padding: 4px 8px; font-size: 13px; font-weight: bold; border-radius: 4px; box-sizing: border-box;">
+            <input type="text" class="constructor-input-ru" autocomplete="off" style="width: 100%; background: #0f172a; color: #cbd5e1; border: 1px solid #334155; padding: 4px 8px; font-size: 11px; border-radius: 4px; box-sizing: border-box;">
+        </div>
             `;
             inputKk = box.querySelector('.constructor-input-kk');
             inputRu = box.querySelector('.constructor-input-ru');
